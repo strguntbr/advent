0d956dd314803d5fec9b81569e38900dfa42ba85
Marcus Winkler <marcus.winkler@gmail.com>
2020-12-01 13:19:01 +0100

    First riddle of day 1

---
+++ solveAllJava.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+javac at/ctrlbreak/advent/*.java
+for file in $(ls at/ctrlbreak/advent/*.java); do
+  riddle=$(echo "$file" | sed 's/\.java$//g' | sed 's/at\/ctrlbreak\/advent\///g')
+  echo -n "$riddle: "
+  java at.ctrlbreak.advent.$riddle
+done
